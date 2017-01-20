f0e77a19151147608914f5775c7391bb61b4c264
AdityaS <adityashingvi13@gmail.com>
2017-01-20 01:16:24 -0800

    1.LL-Remove duplicates without buffer

--- src/com/ds/ctc/LinkedList.java
+++ src/com/ds/ctc/LinkedList.java
@@ -11,12 +11,12 @@ public class LinkedList {
 		Node n1 = new Node(10, null);
 		Node n2 = new Node(5, n1);
 		Node n3 = new Node(3, n2);
-		Node n4 = new Node(1, n3);
+		Node n4 = new Node(6, n3);
 		Node n5 = new Node(5, n4);
 		Node n6 = new Node(6, n5);
 		Node n7 = new Node(0, n6);
 		
-		Node head = list.removeDuplicates(n7);
+		Node head = list.removeDuplicatesNoBuffer(n7);
 		list.printList(head);
 	}
 	
@@ -24,14 +24,14 @@ public class LinkedList {
 	public void printList(Node head) {
 		if (head != null) {
 			Node temp = head;
-			while (temp.next != null) {
+			while (temp != null) {
 				System.out.print(temp.data + " ");
 				temp = temp.next;
 			}
 		}
 	}
 	
-	//remove duplicates
+	//remove duplicates USING BUFFER
 	public Node removeDuplicates(Node head) {
 		Map<Integer, Integer> map = new HashMap<>();
 		Node current = head;
@@ -45,6 +45,28 @@ public class LinkedList {
 				previous = current;
 				current = current.next;
 			}
+		}
+		return head;
+	}
+	
+	//remove duplicates without using buffer
+	public Node removeDuplicatesNoBuffer(Node head) {
+		if (head.next == null) {
+			return head;
+		}
+		Node current = head;
+		Node next, prev;
+		while(current.next != null) {
+			next = current.next;
+			prev = current;
+			while (next != null) {
+				if(current.data == next.data) {
+					prev.next = next.next;
+				}
+				prev = prev.next;
+				next = next.next;
+			}
+			current = current.next;
 		}
 		return head;
 	}
