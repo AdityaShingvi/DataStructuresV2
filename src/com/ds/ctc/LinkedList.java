d71bf38187e928642579590be18bb07e83648fc9
AdityaS <adityashingvi13@gmail.com>
2017-01-24 00:22:45 -0800

    1.Find kth last element 2.Delete from middle of SLL

--- src/com/ds/ctc/LinkedList.java
+++ src/com/ds/ctc/LinkedList.java
@@ -7,7 +7,7 @@ public class LinkedList {
 	public static void main(String[] args) {
 		LinkedList list = new LinkedList();
 		
-		//create a list
+		//create a list 4 -> 10 -> 5 -> 6 -> 2 -> 19 -> 11 -> 1
 		Node n1 = new Node(10, null);
 		Node n2 = new Node(5, n1);
 		Node n3 = new Node(3, n2);
@@ -16,8 +16,14 @@ public class LinkedList {
 		Node n6 = new Node(6, n5);
 		Node n7 = new Node(0, n6);
 		
-		Node head = list.removeDuplicatesNoBuffer(n7);
-		list.printList(head);
+		//Node head = list.removeDuplicatesNoBuffer(n7);
+		//list.printList(head);
+		
+		//int data = list.findKthLastElement(n7, 0);
+		//System.out.println(data);
+		
+		list.deletefromMiddle(n4);
+		list.printList(n7);
 	}
 	
 	//print the complete list
@@ -70,4 +76,47 @@ public class LinkedList {
 		}
 		return head;
 	}
+
+	public int findKthLastElement(Node head, int k) {
+		if (head == null || k <= 0) {
+			return -1;
+		}
+		if (head.next == null) {
+			return head.data;
+		}
+		Node current = head;
+		int pos = k;
+		while (current != null && pos > 0) {
+			current = current.next;
+			pos--;
+		}
+		while (current != null) {
+			head = head.next;
+			current = current.next;
+		}
+		return head.data;
+	}
+
+	public void deletefromMiddle(Node node) {
+		if (node == null) {
+			return;
+		}
+		if (node.next == null) {
+			node = null;
+			return;
+		}
+		Node current = node;
+		Node temp = node.next;
+		while (temp.next != null) {
+			node.data = temp.data;
+			node = temp;
+			temp = temp.next;
+		}
+		node.data = temp.data;
+		node.next = null;
+		temp = null;
+		//printList(current);
+	}
+	
+	//todo partition a linked list around a value x
 }
